96f066ad4552e7fce529b80f69b3af4f75639880
alexandre-rech-lages <alexandre.rech.lages@gmail.com>
2021-06-20 21:53:53 -0300

    Corrigindo nome do primeiro teste

--- eAgenda.Tests/UnitTest1.cs
+++ eAgenda.Tests/UnitTest1.cs
@@ -4,10 +4,10 @@ using System;
 namespace eAgenda.Tests
 {
     [TestClass]
-    public class UnitTest1
+    public class UnitTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestMethod()
         {
         }
     }
